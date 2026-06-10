d0e319168c99cfd4a65c94eb617860b76c77702d
Felipe256 <fcavalcante2013.fc@gmail.com>
2026-06-10 15:00:17 -0300

    Alterando o desconto e a representação

--- coinFlip.py
+++ coinFlip.py
@@ -3,9 +3,9 @@ import ProgramacaoDinamicaMDP as pdMDP
 valorMaximo = 100
 numEstado = {i for i in range(1, valorMaximo)}
 numEstadoParaNome = {estado : "S"+str(estado) for estado in numEstado}
+numEstadoParaNome[0] ="Sderrota"
 copiaEstados = numEstadoParaNome.copy()
 estados = copiaEstados.values()
-numEstadoParaNome[0] ="Sfinal"
 numEstadoParaNome[valorMaximo] ="Sfinal"
 probabilidadeCara = 0.3
 acoes = {numEstadoParaNome[estado]:
@@ -14,10 +14,18 @@ acoes = {numEstadoParaNome[estado]:
                     {1 if estado + aposta == valorMaximo else 0 : 
                         probabilidadeCara}, 
                 numEstadoParaNome[estado - aposta]: 
-                    {1 if estado - aposta == 0 else 0 : 
+                    {0 : 
                         1 - probabilidadeCara}} 
         for aposta in range(0, min(estado, valorMaximo - estado) + 1)} for estado in numEstado}
 acoes["Sderrota"] = {1: {"Sfinal": {0: 1}}}
+for estado in numEstadoParaNome.values():
+    if estado == "Sfinal":
+        continue
+    acoes[estado][0] = {estado: {0: 1}}
 
-print(pdMDP.valueInteration(0.001, estados, acoes, 0, {numEstadoParaNome[i]: {min(50, i, 100-i)} for i in range(1, valorMaximo)}, "Sfinal"))
-#print(pdMDP.iterativePolicyEvaluation(0.001,{estado: {1 : 1} for estado in estados}, estados, acoes, 0, "Sfinal"))
+print("acoes: " + str(acoes))
+politica = {numEstadoParaNome[i]: {0  : 1} for i in range(0, valorMaximo)}
+acoesPolitica = {numEstadoParaNome[i]: {0} for i in range(0, valorMaximo)}
+print("politica: " + str(politica))
+#print(pdMDP.valueInteration(0.001, estados, acoes, 1, acoesPolitica, "Sfinal"))
+print(pdMDP.policyIteration(politica, estados, acoes, 1, "Sfinal"))
