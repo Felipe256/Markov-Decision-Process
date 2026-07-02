94bcad7c1f497edcb84086a2d76f8dd125755e93
Felipe256 <fcavalcante2013.fc@gmail.com>
2026-07-02 09:53:49 -0300

    Mudando para que o valor seja sempre positivo

--- ExponentialUtilityFunctionRSMDP.py
+++ ExponentialUtilityFunctionRSMDP.py
@@ -3,8 +3,8 @@ import numpy as np
 def iterativePolicyEvaluation(lmbda, theta, valorInicialEstados, politica, estados, acoes, desconto, nomeUltimoEstado):
     delta = float("inf")
     values = {estado: valorInicialEstados for estado in estados}
-    values[nomeUltimoEstado] = - np.sign(lmbda)
-    while delta >= theta:
+    values[nomeUltimoEstado] = -1 * np.sign(lmbda) if lmbda < 0 else 1 * np.sign(lmbda)
+    while delta >= theta and not (values["S0"] == float("inf") or values["S0"] == float("-inf")):
         delta = 0
         for estado in estados:
             v = values[estado]
@@ -14,7 +14,7 @@ def iterativePolicyEvaluation(lmbda, theta, valorInicialEstados, politica, estad
                     for recompensa in list(acoes[estado][acao][estado_proximo].keys()):
                         probabilidade = acoes[estado][acao][estado_proximo][recompensa]
                         valorProximoEstado = values[estado_proximo] if estado_proximo != estado else v
-                        values[estado] += (politica[estado][acao] * probabilidade * (np.exp(-lmbda * recompensa) * desconto * valorProximoEstado))
+                        values[estado] += (politica[estado][acao] * probabilidade * (np.exp(lmbda * recompensa) * desconto * valorProximoEstado))
             delta = max(delta, abs(v - values[estado]))
     return values
 
--- drivingLicense.py
+++ drivingLicense.py
@@ -2,7 +2,19 @@ import ProgramacaoDinamicaMDP as pdMDP
 import ExponentialUtilityFunctionRSMDP as EUF
 import PiecewiseLinearTransformationRSMDP as PLT
 import ValueAtRiskRSMDP as VaT
+import numpy as np
 import graficos
+
+def calculaValorPoliticaEUT(politica, comeco, fim, quantidade, estados, acoes, nomeUltimoEstado):
+    intervalo = np.linspace(comeco, fim, num = quantidade)
+    valores = [[] for i in range(len(politica))]
+    cores = ["blue", "green", "red", "cyan", "pink", "black", "blue", "green", "red", "cyan", "pink", "black"]
+    estilosLinha = ["-", "-", "-", "-", "-", "-", "--", "--", "--", "--", "--", "--"]
+    for num in intervalo:
+        for i in range(len(politica)):
+            valorS0 = EUF.iterativePolicyEvaluation(num, 0.00001, 500, politica[i], estados, acoes, 1, nomeUltimoEstado)["S0"]
+            valores[i].append(np.log(valorS0)/ (num if num != 0 else 1))
+    graficos.geraGrafico(intervalo, valores, "Interações de lambda sobre a politica", "lambda", "Valor da politica EUT", cores, estilosLinha)
 
 numEstado = {i for i in range(0,11)}
 numEstadoParaNome = {estado : "S"+str(estado) for estado in numEstado}
@@ -23,14 +35,26 @@ acoes = {numEstadoParaNome[estado] :
             }   
         for estado in numEstado 
         }
-politica = {numEstadoParaNome[i]: {0  : 1} for i in numEstado}
-acoesPolitica = {numEstadoParaNome[i]: {1} for i in numEstado}
-heuristica = {numEstadoParaNome[i]: 1 for i in numEstado}
-heuristica["Sfinal"] = 0
+politicas = [
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {3: 1}, 'S3': {2: 1}, 'S4': {1: 1}, 'S5': {0: 1}, 'S6': {0: 1}, 'S7': {0: 1}, 'S8': {0: 1}, 'S9': {0: 1}, 'S10': {0: 1}} 
+    ,{'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {3: 1}, 'S4': {2: 1}, 'S5': {1: 1}, 'S6': {0: 1}, 'S7': {0: 1}, 'S8': {0: 1}, 'S9': {0: 1}, 'S10': {0: 1}}, 
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {3: 1}, 'S5': {2: 1}, 'S6': {1: 1}, 'S7': {0: 1}, 'S8': {0: 1}, 'S9': {0: 1}, 'S10': {0: 1}}, 
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {4: 1}, 'S5': {3: 1}, 'S6': {2: 1}, 'S7': {1: 1}, 'S8': {0: 1}, 'S9': {0: 1}, 'S10': {0: 1}}, 
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {4: 1}, 'S5': {4: 1}, 'S6': {3: 1}, 'S7': {2: 1}, 'S8': {1: 1}, 'S9': {0: 1}, 'S10': {0: 1}}, 
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {4: 1}, 'S5': {4: 1}, 'S6': {4: 1}, 'S7': {3: 1}, 'S8': {2: 1}, 'S9': {1: 1}, 'S10': {0: 1}}, 
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {4: 1}, 'S5': {4: 1}, 'S6': {4: 1}, 'S7': {4: 1}, 'S8': {3: 1}, 'S9': {2: 1}, 'S10': {1: 1}}, 
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {4: 1}, 'S5': {4: 1}, 'S6': {4: 1}, 'S7': {4: 1}, 'S8': {4: 1}, 'S9': {3: 1}, 'S10': {2: 1}} 
+    ,{'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {4: 1}, 'S5': {4: 1}, 'S6': {4: 1}, 'S7': {4: 1}, 'S8': {4: 1}, 'S9': {3: 1}, 'S10': {1: 1}}, 
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {4: 1}, 'S5': {4: 1}, 'S6': {4: 1}, 'S7': {4: 1}, 'S8': {4: 1}, 'S9': {3: 1}, 'S10': {2: 1}}, 
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {4: 1}, 'S5': {4: 1},'S6': {4: 1}, 'S7': {4: 1}, 'S8': {4: 1}, 'S9': {4: 1}, 'S10': {3: 1}}, 
+    {'S0': {4: 1}, 'S1': {4: 1}, 'S2': {4: 1}, 'S3': {4: 1}, 'S4': {4: 1}, 'S5': {4: 1}, 'S6': {4: 1}, 'S7': {4: 1}, 'S8': {4: 1}, 'S9': {4: 1}, 'S10': {4: 1}}
+    ]
 #graficos.mostraGraficos(pdMDP.costPolicyIteration(500, politica, estados, acoes, 1, "Sfinal"))
 
-lmbda = [0.7, 0.5, 0.4, 0.3, 0.2, 0.1, 0, -0.1, -0.2, -0.3, -0.4, -0.5, -0.6]
-{ print(str(lb)+" "+str(EUF.policyIteration(lb, 0.00001, 500, politica, estados, acoes, 1, "Sfinal")[0])+"\n")  for lb in lmbda }
+calculaValorPoliticaEUT(politicas, -0.8, 0.8, 1600, estados, acoes, "Sfinal")
+
+#mbda = [0.7, 0.5, 0.4, 0.3, 0.2, 0.1, 0, -0.1, -0.2, -0.3, -0.4, -0.5, -0.6]
+#{ print(str(lb)+" "+str(EUF.policyIteration(lb, 0.00001, 500, politica, estados, acoes, 1, "Sfinal")[0])+"\n")  for lb in lmbda }
 
 '''kPLT = -0.2
 convergencia = PLT.policyIteration(kPLT, 500, politica, estados, acoes, 1, "Sfinal")
